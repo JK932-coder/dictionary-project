3254db21f964bceb7932e90c2bdb653100f25dfe
JK932-coder <ekaterina.kosmaciova@gmail.com>
2025-04-10 15:50:28 +0100

    Phonetics added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import Dictionary from "./Dictionary"
-function App() {
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Jekaterina Valeviciene</small>
+          <small>Coded by SheCodes</small>
         </footer>
       </div>
     </div>
   );
 }
-
-export default App;
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,37 +1,34 @@
-import React, {useState} from "react";
-import "./Dictionary.css";
+import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
-import logo from "./logo.png";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword,setKeyword]= useState(""); 
-    let [results,setResults]=useState(null);
-    
-    function handleResponse(response){
-console.log(response.data[0]);
-setResults(response.data[0]);
-    }
-    function handleKeywordChange(event){
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
-    }
-    function search (event) {
-        event.preventDefault();
+  }
 
-   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`; 
-axios.get(apiUrl).then(handleResponse);
-
-}
-   
-    return (
-      <div className="Dictionary">
-        <img src={logo} alt="Logo" className="dictionary-image" />
-        <h4>Enter the word</h4>
-        <form onSubmit={search}>
-          <input type="search" onChange={handleKeywordChange} />
-        </form>
-        <Results results={results} />
-      </div>
-    );
-
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
