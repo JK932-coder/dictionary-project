39a84e9517ecdbe3278ff4ac621af714e410f82b
JK932-coder <ekaterina.kosmaciova@gmail.com>
2025-04-15 18:21:45 +0100

    Added photos and tidied up the styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+ let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
-
+ function handlePexelsResponse(response) {
+   setPhotos(response.data.photos);
+ }
   function search() {
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+   let pexelsApiKey ="Q47zQzSH3dZfzHyVNmIB597DeFlQ5p1nwa2NcwhmuAJawSSQPGYC2Uxo";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -37,6 +45,8 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h3>
+            Type the word to look up</h3>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
@@ -44,9 +54,10 @@ export default function Dictionary(props) {
               onChange={handleKeywordChange}
             />
           </form>
-          <div className="hint">suggested words:sunset, town,wine,yoga</div>
+          <div className="hint">i.e:sunset, town,wine,yoga</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   
